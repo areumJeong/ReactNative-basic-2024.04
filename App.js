4de03ac459610daad6fd7ca688968a371ca284df
areumJeong <add5833@naver.com>
2024-04-01 15:14:43 +0900

    React Native

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 import HelloWorld from './components/HelloWorld';
+import Cat from './components/Cat';
+import State from './components/State';
+import PizzaTranslator from './components/PizzaTranslator';
+import FlatListBasic from './components/FlatListBasic';
+import SectionListBasic from './components/SectionListBasic';
+import FetchExample from './components/FetchExample';
+
+const logo = { uri: 'https://reactnative.dev/img/tiny_logo.png', width: 32, height: 32, };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HelloWorld></HelloWorld>
-    </View>
+    <ScrollView style={{paddingTop: 24, paddingLeft: 2}}>
+      <Image source={logo} />
+      <FetchExample />
+      <Image source={logo} />
+      <SectionListBasic />
+      <Image source={logo} />
+      <FlatListBasic />
+      <Image source={logo} />
+      <PizzaTranslator />
+      <Image source={logo} />
+      <State name='Run Tum Tugger' />
+      <Image source={logo} />
+      <Cat name='Maru' num='1' />
+      <Cat name='Run Tum Tugger' num='2' />
+      <Image source={logo} />
+      <HelloWorld />
+    </ScrollView>
   );
 }
 
